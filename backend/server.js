3bc2f2611b4af8d3adba8c51d308aaf8b3d79e2b
Niraj Khadka <khadka.niraj11111@gmail.com>
2024-07-09 13:46:28 -0400

    added request middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./connections/db');
+const responseLogger = require('./middlewares/logger');
+
 const app = express();
 const port = process.env.PORT || 8000;
 
+// Middlewares
+app.use(responseLogger);
 
 app.get("/",function(req,res){
     res.json({"message":"Welcome to Bulk Email Processor Service"})
