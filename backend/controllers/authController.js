4a2f7f03257117d10f5846ecf85e6d73e319f246
Niraj Khadka <khadka.niraj11111@gmail.com>
2024-07-12 17:37:01 +0545

    string changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -60,7 +60,7 @@ const login = async (req, res) => {
     const { username, password } = req.body;
     const user = await getUserByUsernameOrEmail(username);
     if(!user){
-      return res.status(404).json(errorResponse("s", 400));
+      return res.status(404).json(errorResponse("Invalid Credentials", 400));
     }
     const isPasswordMatch = await user.comparePassword(password);
     if(!isPasswordMatch){
